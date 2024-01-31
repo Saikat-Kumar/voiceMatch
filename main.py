f5cd56b82bc543db9b163bbb6696f421b1e35b9e
Saikat Kumar <59053759+Saikat-Kumar@users.noreply.github.com>
2024-02-01 00:40:03 +0530

    changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import glob
+import json
 import sys
 
 import sqlite3
@@ -12,9 +13,50 @@ import shutil
 import string
 import random
 import os
-path=''
-from speechbrain.pretrained import SpeakerRecognition
+import torchaudio
+from speechbrain.pretrained import EncoderClassifier
 
+path=''
+name=''
+import sqlite3
+conn = sqlite3.connect('DB.db')
+sql = 'create table if not exists voice  (name VARCHAR(255) NOT NULL,embedding TEXT NOT NULL)'
+conn.execute(sql)
+from speechbrain.pretrained import SpeakerRecognition
+class UploadThread(QThread):
+    setlogUpload = Signal(str)
+    def __init__(self, parent=None):
+        super(QThread, self).__init__()
+    def run(self):
+        global  path
+        print(path)
+        conn = sqlite3.connect('DB.db')
+        self.setlogUpload.emit('Path ='+path[0])
+        self.setlogUpload.emit('classifier Load ')
+        classifier = EncoderClassifier.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb",
+                                                    savedir="pretrained_models/spkrec-ecapa-voxceleb")
+        classifier.hparams.label_encoder.ignore_len()
+        self.setlogUpload.emit('Audio Load ')
+        # Compute embeddings
+        signal, fs = torchaudio.load(path[0])
+        self.setlogUpload.emit('Embedding Start....')
+        embeddings = classifier.encode_batch(signal)
+        self.setlogUpload.emit('Embedding Complete')
+        listEm=embeddings[0][0].tolist()
+        sql_as_text = json.dumps(listEm)
+
+        global name
+        try:
+            conn.execute("INSERT INTO voice (name,embedding) VALUES (?, ?)",(name, sql_as_text))
+        except sqlite3.Error as er:
+            print('SQLite error: %s' % (' '.join(er.args)))
+
+        self.setlogUpload.emit('Database Saved')
+
+
+
+    def stop(self):
+        print('stop')
 class MatchThread(QThread):
     setprogress = Signal(int)
     setlog = Signal(str)
@@ -25,8 +67,6 @@ class MatchThread(QThread):
         global  path
         print(path)
         count=0
-        for filename in glob.glob(os.path.join('./upload/', '*.wav')):
-            count=count+1
         self.setprogress.emit(0)
         perslot=100/count
         pbarValue=0
@@ -34,18 +74,22 @@ class MatchThread(QThread):
         highestFilename = 0
         verification = SpeakerRecognition.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb",
                                                        savedir="pretrained_models/spkrec-ecapa-voxceleb")
-        for filename in glob.glob(os.path.join('./upload/', '*.wav')):
-            pbarValue = pbarValue+perslot
-
-            print(path,filename.replace("\\", "/"))
-            score, prediction = verification.verify_files(path,
-                                                          filename.replace("\\", "/"))
-            # Different Speakers
-            if(highest<score[0]):
-                highest=score[0]
-                highestFilename=filename
-            self.setprogress.emit(int(pbarValue))
-            self.setlog.emit(filename)
+        conn = sqlite3.connect('DB.db')
+        conn.execute('SELECT * FROM voice')
+        # for row in conn:
+        #     self.setlog.emit(row)
+        # for filename in glob.glob(os.path.join('./upload/', '*.wav')):
+        #     pbarValue = pbarValue+perslot
+        #
+        #     print(path,filename.replace("\\", "/"))
+        #     score, prediction = verification.verify_files(path,
+        #                                                   filename.replace("\\", "/"))
+        #     # Different Speakers
+        #     if(highest<score[0]):
+        #         highest=score[0]
+        #         highestFilename=filename
+        #     self.setprogress.emit(int(pbarValue))
+        #     self.setlog.emit(filename)
         self.setMatch.emit(highestFilename)
     def stop(self):
         print('stop')
@@ -57,30 +101,22 @@ class mainWindow(QMainWindow,mainui.Ui_MainWindow ):
         self.setupUi(self)
         self.search.clicked.connect(self.defMatch)
         self.upload.clicked.connect(self.defCopy)
+        self.submitUpload.clicked.connect(self.defsubmitUplaod)
+        self.threadupload = UploadThread()
+        self.threadupload.setlogUpload.connect(self.defsetlogupload)
         self.threadmatch = MatchThread()
         self.threadmatch.setprogress.connect(self.defsetprogress)
         self.threadmatch.setlog.connect(self.defsetlog)
         self.threadmatch.setMatch.connect(self.defsetMatch)
 
     def defCopy(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file',
-                                            '', "Audio files (*.wav )")
-        # start the thread
+        fname = QFileDialog.getOpenFileName(self, 'Open file','', "Audio files (*.wav )")
         global path
-        path = fname[0]
-        print(path)
-        N = 7
-        res = str(''.join(random.choices(string.ascii_uppercase +
-                                         string.digits, k=N)))
-        print(res)
-        shutil.copyfile(path, './upload/'+res+'.wav')
-        msg = QMessageBox()
-        msg.setIcon(QMessageBox.Information)
-        msg.setText("Wav uploaded")
-        msg.setInformativeText("")
-        msg.setWindowTitle("MessageBox")
-        msg.setDetailedText("Wav file upload Complete")
-        msg.show()
+        path=fname
+    def defsubmitUplaod(self):
+        global name
+        name = self.textEdit.toPlainText()
+        self.threadupload.start()
     def defMatch(self):
         fname = QFileDialog.getOpenFileName(self, 'Open file',
                                             '', "Audio files (*.wav )")
@@ -94,6 +130,8 @@ class mainWindow(QMainWindow,mainui.Ui_MainWindow ):
         self.log.addItem(txt)
     def defsetMatch(self,txt):
         self.Matched.setText(txt)
+    def defsetlogupload(self,txt):
+        self.logupload.addItem(txt)
 
 if __name__ == "__main__":
 
--- test.py
+++ test.py
@@ -1,6 +1,20 @@
 import glob
+import json
 import shutil
 from speechbrain.pretrained import SpeakerRecognition
-verification = SpeakerRecognition.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb", savedir="pretrained_models/spkrec-ecapa-voxceleb")
-score, prediction = verification.verify_files("D:/VoiceData/WBSWAN/Amir_Mayureswar1_Bhq_Birbhum.wav","./upload/115EVYW.wav") # Different Speakers
-print(score[0])
+import sqlite3
+import torchaudio
+from speechbrain.pretrained import EncoderClassifier
+
+classifier = EncoderClassifier.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb",savedir="pretrained_models/spkrec-ecapa-voxceleb")
+classifier.hparams.label_encoder.ignore_len()
+# Compute embeddings
+signal, fs = torchaudio.load('D:\VoiceData\dev-clean\84/1.wav')
+embeddings = classifier.encode_batch(signal)
+listEm=embeddings[0][0].tolist()
+sql_as_text = json.dumps(listEm)
+conn = sqlite3.connect('DB.db')
+conn.execute(
+            "INSERT INTO voice (name,embedding) VALUES (?, ?)",
+            ('test', sql_as_text))
+print('done')
